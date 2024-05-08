ff66801393d614cfdfeceff41c5e95185fb66912
Martin Gondermann <martin.gondermann@bayoo.net>
2024-05-08 08:13:21 +0200

    Fix cmp mapping on command line

--- config/plugins/coding/cmp.nix
+++ config/plugins/coding/cmp.nix
@@ -173,12 +173,6 @@
       })
 
       -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline({'/', "?" }, {
-        sources = {
-          { name = 'buffer' }
-        }
-      })
-
       -- Set configuration for specific filetype.
       cmp.setup.filetype('gitcommit', {
         sources = cmp.config.sources(
@@ -191,14 +185,25 @@
         )
       })
 
+      cmp.setup.cmdline({'/', "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
       -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
       cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources(
           {
             { name = 'path' }
           },
           {
-            { name = 'cmdline' }
+            {
+              name = 'cmdline',
+              options = { ignore_cmds = { 'Man', '!' } }
+            },
           }
         ),
       })
