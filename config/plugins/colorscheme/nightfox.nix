1da34889a0904183d78387981f5ec152577549a1
Martin Gondermann <martin.gondermann@bayoo.net>
2024-06-06 15:49:05 +0200

    Add option to override theme

--- config/plugins/colorscheme/nightfox.nix
+++ config/plugins/colorscheme/nightfox.nix
@@ -1,9 +1,21 @@
-{pkgs, ...}: {
-  extraPlugins = [pkgs.vimPlugins.nightfox-nvim];
-  colorscheme = "nightfox";
-  extraConfigLua =
-    # lua
-    ''
-      require("nightfox").setup({ options = { transparent = false } })
-    '';
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options.theme = lib.mkOption {
+    default = "nightfox";
+    type = lib.types.enum ["nightfox" "dayfox" "duskfox" "dawnfox"];
+  };
+
+  config = {
+    extraPlugins = [pkgs.vimPlugins.nightfox-nvim];
+    colorscheme = "${config.theme}";
+    extraConfigLua =
+      # lua
+      ''
+        require("nightfox").setup({ options = { transparent = false } })
+      '';
+  };
 }
