c446673559475f01bfd3cb72f2159161e74eb1e4
Martin Gondermann <magicmonty@pagansoft.de>
2024-08-13 09:30:14 +0200

    Add neotest for rust

--- config/plugins/lang/rust/default.nix
+++ config/plugins/lang/rust/default.nix
@@ -1,5 +1,6 @@
 _: {
   plugins = {
+    neotest.enable = true;
     rustaceanvim = {
       enable = true;
       settings = {
@@ -19,4 +20,59 @@ _: {
       };
     };
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>ur";
+      action.__raw =
+        #lua
+        ''
+          function()
+            require("neotest").run.run(vim.fn.expand("%"))
+            end
+        '';
+      options = {desc = "Run all tests in file";};
+    }
+    {
+      mode = "n";
+      key = "<leader>ut";
+      action.__raw =
+        #lua
+        ''
+          function()
+            require("neotest").run.run()
+            end
+        '';
+      options = {desc = "Run nearest test";};
+    }
+    {
+      mode = "n";
+      key = "<leader>uw";
+      action.__raw =
+        #lua
+        ''
+          function()
+            require("neotest").watch.toggle()
+            end
+        '';
+      options = {desc = "Toggle test watcher";};
+    }
+    {
+      mode = "n";
+      key = "<leader>uu";
+      action = "<cmd>Neotest run latest<cr>";
+      options = {desc = "Run latest tests";};
+    }
+  ];
+
+  extraConfigLua =
+    #lua
+    ''
+      require("neotest").setup({
+        adapters = {
+          rust = require("rustaceanvim.neotest")
+        }
+      })
+    '';
 }
