6f9fbad3765a09c635d0a327f818d0ed023b2658
Martin Gondermann <magicmonty@pagansoft.de>
2025-01-15 19:57:43 +0100

    Update lua injection in kuala

--- config/plugins/coding/kulala.nix
+++ config/plugins/coding/kulala.nix
@@ -5,47 +5,49 @@
     };
   };
 
-  extraFiles."ftplugin/http.lua".text = ''
-    vim.api.nvim_buf_set_keymap(
-      0,
-      "n",
-      "<CR>",
-      "<cmd>lua require('kulala').run()<cr>",
-      { noremap = true, silent = true, desc = "Execute the request" }
-    )
+  extraFiles."ftplugin/http.lua".text =
+    # lua
+    ''
+      vim.api.nvim_buf_set_keymap(
+        0,
+        "n",
+        "<CR>",
+        "<cmd>lua require('kulala').run()<cr>",
+        { noremap = true, silent = true, desc = "Execute the request" }
+      )
 
-    vim.api.nvim_buf_set_keymap(
-      0,
-      "n",
-      "ö",
-      "<cmd>lua require('kulala').jump_prev()<cr>",
-      { noremap = true, silent = true, desc = "Jump to the previous request" }
-    )
+      vim.api.nvim_buf_set_keymap(
+        0,
+        "n",
+        "ö",
+        "<cmd>lua require('kulala').jump_prev()<cr>",
+        { noremap = true, silent = true, desc = "Jump to the previous request" }
+      )
 
-    vim.api.nvim_buf_set_keymap(
-      0,
-      "n",
-      "ä",
-      "<cmd>lua require('kulala').jump_next()<cr>",
-      { noremap = true, silent = true, desc = "Jump to the next request" }
-    )
+      vim.api.nvim_buf_set_keymap(
+        0,
+        "n",
+        "ä",
+        "<cmd>lua require('kulala').jump_next()<cr>",
+        { noremap = true, silent = true, desc = "Jump to the next request" }
+      )
 
-    vim.api.nvim_buf_set_keymap(
-      0,
-      "n",
-      "<leader>i",
-      "<cmd>lua require('kulala').inspect()<cr>",
-      { noremap = true, silent = true, desc = "Inspect the current request" }
-    )
+      vim.api.nvim_buf_set_keymap(
+        0,
+        "n",
+        "<leader>i",
+        "<cmd>lua require('kulala').inspect()<cr>",
+        { noremap = true, silent = true, desc = "Inspect the current request" }
+      )
 
-    vim.api.nvim_buf_set_keymap(
-      0,
-      "n",
-      "<leader>t",
-      "<cmd>lua require('kulala').toggle_view()<cr>",
-      { noremap = true, silent = true, desc = "Toggle between body and headers" }
-    )
-  '';
+      vim.api.nvim_buf_set_keymap(
+        0,
+        "n",
+        "<leader>t",
+        "<cmd>lua require('kulala').toggle_view()<cr>",
+        { noremap = true, silent = true, desc = "Toggle between body and headers" }
+      )
+    '';
 
   plugins.kulala = {
     enable = true;
