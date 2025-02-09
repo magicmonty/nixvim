a1d8d6b8fcce93ad334d206b0673d74a688bd2a4
Martin Gondermann <magicmonty@pagansoft.de>
2025-02-09 10:24:40 +0100

    Update flake, disable neotest

---
+++ config/plugins/coding/neotest.nix
@@ -0,0 +1,72 @@
+{
+  config,
+  lib,
+}:
+with lib; {
+  plugins = {
+    neotest.enable = false;
+  };
+
+  keymaps = mkIf config.plugins.neotest.enable [
+    {
+      mode = "n";
+      key = "<leader>ctf";
+      action.__raw =
+        #lua
+        ''
+          function()
+            require("neotest").run.run(vim.fn.expand("%"))
+          end
+        '';
+      options = {desc = "Run all tests in file";};
+    }
+    {
+      mode = "n";
+      key = "<leader>ctr";
+      action.__raw =
+        #lua
+        ''
+          function()
+            require("neotest").run.run()
+          end
+        '';
+      options = {desc = "Run nearest test";};
+    }
+    {
+      mode = "n";
+      key = "<leader>ctw";
+      action.__raw =
+        #lua
+        ''
+          function()
+            require("neotest").watch.toggle()
+          end
+        '';
+      options = {desc = "Toggle test watcher";};
+    }
+    {
+      mode = "n";
+      key = "<leader>ctt";
+      action.__raw =
+        #lua
+        ''
+          function()
+            require("neotest").run.run_last()
+          end
+        '';
+      options = {desc = "Run latest tests";};
+    }
+    {
+      mode = "n";
+      key = "<leader>cts";
+      action.__raw =
+        #lua
+        ''
+          function()
+            require("neotest").summary.toggle()
+          end
+        '';
+      options = {desc = "Neotest: Toggle summary pane";};
+    }
+  ];
+}
--- config/plugins/lang/rust/default.nix
+++ config/plugins/lang/rust/default.nix
@@ -1,6 +1,9 @@
-_: {
+{
+  config,
+  lib,
+}:
+with lib; {
   plugins = {
-    neotest.enable = true;
     rustaceanvim = {
       enable = true;
       settings = {
@@ -21,70 +24,8 @@ _: {
     };
   };
 
-  keymaps = [
-    {
-      mode = "n";
-      key = "<leader>ctf";
-      action.__raw =
-        #lua
-        ''
-          function()
-            require("neotest").run.run(vim.fn.expand("%"))
-          end
-        '';
-      options = {desc = "Run all tests in file";};
-    }
-    {
-      mode = "n";
-      key = "<leader>ctr";
-      action.__raw =
-        #lua
-        ''
-          function()
-            require("neotest").run.run()
-          end
-        '';
-      options = {desc = "Run nearest test";};
-    }
-    {
-      mode = "n";
-      key = "<leader>ctw";
-      action.__raw =
-        #lua
-        ''
-          function()
-            require("neotest").watch.toggle()
-          end
-        '';
-      options = {desc = "Toggle test watcher";};
-    }
-    {
-      mode = "n";
-      key = "<leader>ctt";
-      action.__raw =
-        #lua
-        ''
-          function()
-            require("neotest").run.run_last()
-          end
-        '';
-      options = {desc = "Run latest tests";};
-    }
-    {
-      mode = "n";
-      key = "<leader>cts";
-      action.__raw =
-        #lua
-        ''
-          function()
-            require("neotest").summary.toggle()
-          end
-        '';
-      options = {desc = "Neotest: Toggle summary pane";};
-    }
-  ];
-
   extraConfigLuaPost =
+    mkIf config.plugins.neotest.enable
     #lua
     ''
       require('neotest').setup({
