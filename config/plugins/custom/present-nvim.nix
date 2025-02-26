1c674b3d2da33f3581c606b9d4c74b83d05a9dcd
Martin Gondermann <martin.gondermann@bayoo.net>
2025-02-26 14:00:31 +0100

    update present.nvim

--- config/plugins/custom/present-nvim.nix
+++ config/plugins/custom/present-nvim.nix
@@ -5,8 +5,8 @@
       src = pkgs.fetchFromGitHub {
         owner = "magicmonty";
         repo = "present.nvim";
-        rev = "1682e66fadde429360d695e773b2f67fb2b2e013";
-        hash = "sha256-VxZjS2dLpa+/4zw8s/oQGaXDwVCvJTq3xN7hUZgJIBo=";
+        rev = "ec81560a8c5330400d557f0b48908721814ea44d";
+        hash = "sha256-1gl6f/v3ez88Ap8VCC6r5WCSmFy4dunEH6Q8n04b6mc=";
       };
     })
   ];
