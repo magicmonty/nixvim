811f4652f2f061aa4a52f9bc77da9b3acfe22bc5
Martin Gondermann <magicmonty@pagansoft.de>
2025-05-07 23:17:32 +0200

    Update kulala

--- config/plugins/coding/kulala.nix
+++ config/plugins/coding/kulala.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   filetype = {
     extension = {
       http = "http";
@@ -12,7 +16,7 @@
         0,
         "n",
         "<CR>",
-        "<cmd>lua require('kulala').run()<cr>",
+        "<cmd>lua require('kulala').run()<CR>",
         { noremap = true, silent = true, desc = "Execute the request" }
       )
 
@@ -20,7 +24,7 @@
         0,
         "n",
         "ö",
-        "<cmd>lua require('kulala').jump_prev()<cr>",
+        "<cmd>lua require('kulala').jump_prev()<CR>",
         { noremap = true, silent = true, desc = "Jump to the previous request" }
       )
 
@@ -28,7 +32,7 @@
         0,
         "n",
         "ä",
-        "<cmd>lua require('kulala').jump_next()<cr>",
+        "<cmd>lua require('kulala').jump_next()<CR>",
         { noremap = true, silent = true, desc = "Jump to the next request" }
       )
 
@@ -36,7 +40,7 @@
         0,
         "n",
         "<leader>i",
-        "<cmd>lua require('kulala').inspect()<cr>",
+        "<cmd>lua require('kulala').inspect()<CR>",
         { noremap = true, silent = true, desc = "Inspect the current request" }
       )
 
@@ -44,54 +48,71 @@
         0,
         "n",
         "<leader>t",
-        "<cmd>lua require('kulala').toggle_view()<cr>",
+        "<cmd>lua require('kulala').toggle_view()<CR>",
         { noremap = true, silent = true, desc = "Toggle between body and headers" }
       )
 
       vim.api.nvim_buf_set_keymap(
         0,
         "n",
+        "<leader>Rc",
+        "<cmd>lua require('kulala').copy()<CR>",
+        { noremap = true, silent = true, desc = "Copy as cURL" }
+      )
+
+      vim.api.nvim_buf_set_keymap(
+        0,
+        "n",
         "<leader>Re",
-        "<cmd>lua require('kulala').set_selected_env()<cr>",
+        "<cmd>lua require('kulala').set_selected_env()<CR>",
         { noremap = true, silent = true, desc = "Select environment" }
       )
 
       vim.api.nvim_buf_set_keymap(
         0,
         "n",
-        "<leader>Ru",
-        "<cmd>lua require('kulala.ui.auth_manager').open_auth_config()<cr>",
-       { noremap = true, silent = true, desc = "Manage auth config" }
+        "<leader>Ra",
+        "<cmd>lua require('kulala.ui.auth_manager').open_auth_config()<CR>",
+        { noremap = true, silent = true, desc = "Manage auth config" }
       )
 
       vim.api.nvim_buf_set_keymap(
         0,
         "n",
         "<leader>Rx",
-        "<cmd>lua require('kulala').scripts_clear_global()<cr>",
-       { noremap = true, silent = true, desc = "Clear globals" }
+        "<cmd>lua require('kulala').scripts_clear_global()<CR>",
+        { noremap = true, silent = true, desc = "Clear globals" }
       )
 
       vim.api.nvim_buf_set_keymap(
         0,
         "n",
         "<leader>RX",
-        "<cmd>lua require('kulala').clear_cached_files()<cr>",
-       { noremap = true, silent = true, desc = "Clear cached files" }
+        "<cmd>lua require('kulala').clear_cached_files()<CR>",
+        { noremap = true, silent = true, desc = "Clear cached files" }
       )
-
-
     '';
 
   plugins.kulala = {
     enable = true;
+
+    package = pkgs.vimUtils.buildVimPlugin {
+      name = "kulala-nvim";
+      src = pkgs.fetchFromGitHub {
+        owner = "magicmonty";
+        repo = "kulala.nvim";
+        rev = "408b482c9ab0b77ff421928278118884d7bf0383";
+        hash = "sha256-nVw+/APADdeL6URkOHfFR0DUkLFS5l1EDcnnfNPzeUg="; # lib.fakeHash;
+      };
+      doCheck = false;
+    };
 
     settings = {
       curl_path = "${pkgs.curl}/bin/curl";
       contenttypes = {
         "application/json" = {
           ft = "json";
-          formatter = ["jq" "."];
+          formatter = ["${pkgs.jq}/bin/jq" "."];
           pathresolver = ''require("kulala.parser.jsonpath").parse'';
         };
         "application/xml" = {
