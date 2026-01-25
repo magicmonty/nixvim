1f67a88997e383fa3c4d87df44b6d2760525463f
Martin Gondermann <magicmonty@pagansoft.de>
2026-01-25 10:16:15 +0100

    rename qmk layout

--- config/plugins/coding/qmk.nix
+++ config/plugins/coding/qmk.nix
@@ -2,7 +2,7 @@
   plugins.qmk = {
     enable = true;
     settings = {
-      name = "LAYOUT_COSMONTYL";
+      name = "LAYOUT_magicmonty";
       variant = "qmk";
       layout = [
         "x x x x x x _ _ _ _ _ x x x x x x"
